e9a7c34df4c2ae93167a524995cb5425e87d7ef1
Hosein mosavy <marscode.csharp.program@gmail.com>
2025-10-14 01:37:47 +0330

    add control box

--- Mars_Languge/Main/Forms/frm_Writing_code_for_run.Designer.cs
+++ Mars_Languge/Main/Forms/frm_Writing_code_for_run.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_Writing_code_for_run));
             this.txtCode = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.btnRun = new System.Windows.Forms.Button();
@@ -37,6 +39,16 @@
             this.txtError = new System.Windows.Forms.RichTextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.pnl_conntrol_box = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_Minimize = new System.Windows.Forms.Button();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.lbl_time = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pnl_conntrol_box.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // txtCode
@@ -44,10 +56,10 @@
             this.txtCode.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.txtCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtCode.ForeColor = System.Drawing.Color.White;
-            this.txtCode.Location = new System.Drawing.Point(41, 55);
+            this.txtCode.Location = new System.Drawing.Point(24, 154);
             this.txtCode.Multiline = true;
             this.txtCode.Name = "txtCode";
-            this.txtCode.Size = new System.Drawing.Size(360, 269);
+            this.txtCode.Size = new System.Drawing.Size(440, 269);
             this.txtCode.TabIndex = 0;
             // 
             // label1
@@ -55,7 +67,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(38, 35);
+            this.label1.Location = new System.Drawing.Point(21, 134);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(82, 17);
             this.label1.TabIndex = 1;
@@ -65,7 +77,7 @@
             // 
             this.btnRun.BackColor = System.Drawing.Color.Silver;
             this.btnRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRun.Location = new System.Drawing.Point(407, 55);
+            this.btnRun.Location = new System.Drawing.Point(504, 180);
             this.btnRun.Name = "btnRun";
             this.btnRun.Size = new System.Drawing.Size(75, 269);
             this.btnRun.TabIndex = 2;
@@ -77,7 +89,7 @@
             // 
             this.txtOutput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.txtOutput.ForeColor = System.Drawing.Color.White;
-            this.txtOutput.Location = new System.Drawing.Point(41, 353);
+            this.txtOutput.Location = new System.Drawing.Point(24, 452);
             this.txtOutput.Name = "txtOutput";
             this.txtOutput.Size = new System.Drawing.Size(440, 220);
             this.txtOutput.TabIndex = 3;
@@ -88,7 +100,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(38, 333);
+            this.label2.Location = new System.Drawing.Point(21, 432);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(59, 17);
             this.label2.TabIndex = 4;
@@ -98,9 +110,9 @@
             // 
             this.txt_lexser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.txt_lexser.ForeColor = System.Drawing.Color.White;
-            this.txt_lexser.Location = new System.Drawing.Point(498, 55);
+            this.txt_lexser.Location = new System.Drawing.Point(836, 629);
             this.txt_lexser.Name = "txt_lexser";
-            this.txt_lexser.Size = new System.Drawing.Size(190, 509);
+            this.txt_lexser.Size = new System.Drawing.Size(190, 103);
             this.txt_lexser.TabIndex = 5;
             this.txt_lexser.Text = "";
             // 
@@ -108,7 +120,7 @@
             // 
             this.txtError.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.txtError.ForeColor = System.Drawing.Color.White;
-            this.txtError.Location = new System.Drawing.Point(694, 55);
+            this.txtError.Location = new System.Drawing.Point(622, 194);
             this.txtError.Name = "txtError";
             this.txtError.Size = new System.Drawing.Size(190, 509);
             this.txtError.TabIndex = 6;
@@ -119,7 +131,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(495, 35);
+            this.label3.Location = new System.Drawing.Point(833, 609);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(51, 17);
             this.label3.TabIndex = 7;
@@ -130,18 +142,107 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(691, 35);
+            this.label4.Location = new System.Drawing.Point(619, 174);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(74, 17);
             this.label4.TabIndex = 8;
             this.label4.Text = "Error List :";
+            // 
+            // pnl_conntrol_box
+            // 
+            this.pnl_conntrol_box.BackColor = System.Drawing.Color.Gray;
+            this.pnl_conntrol_box.Controls.Add(this.label6);
+            this.pnl_conntrol_box.Controls.Add(this.lbl_time);
+            this.pnl_conntrol_box.Controls.Add(this.pictureBox1);
+            this.pnl_conntrol_box.Controls.Add(this.label5);
+            this.pnl_conntrol_box.Controls.Add(this.btn_Minimize);
+            this.pnl_conntrol_box.Controls.Add(this.btn_close);
+            this.pnl_conntrol_box.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_conntrol_box.Location = new System.Drawing.Point(0, 0);
+            this.pnl_conntrol_box.Name = "pnl_conntrol_box";
+            this.pnl_conntrol_box.Size = new System.Drawing.Size(1080, 42);
+            this.pnl_conntrol_box.TabIndex = 9;
+            this.pnl_conntrol_box.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(19, 10);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(130, 23);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Mars Languge";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Mars_Languge.Properties.Resources.Mars;
+            this.pictureBox1.Location = new System.Drawing.Point(162, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(38, 33);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_Minimize
+            // 
+            this.btn_Minimize.BackgroundImage = global::Mars_Languge.Properties.Resources.icons8_minimize_50;
+            this.btn_Minimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_Minimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Minimize.ForeColor = System.Drawing.Color.Gray;
+            this.btn_Minimize.Location = new System.Drawing.Point(983, 4);
+            this.btn_Minimize.Name = "btn_Minimize";
+            this.btn_Minimize.Size = new System.Drawing.Size(43, 35);
+            this.btn_Minimize.TabIndex = 1;
+            this.btn_Minimize.UseVisualStyleBackColor = true;
+            this.btn_Minimize.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_close
+            // 
+            this.btn_close.BackgroundImage = global::Mars_Languge.Properties.Resources.icons8_close_window_50;
+            this.btn_close.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_close.ForeColor = System.Drawing.Color.Gray;
+            this.btn_close.Location = new System.Drawing.Point(1032, 4);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(43, 35);
+            this.btn_close.TabIndex = 0;
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // lbl_time
+            // 
+            this.lbl_time.AutoSize = true;
+            this.lbl_time.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_time.Location = new System.Drawing.Point(549, 10);
+            this.lbl_time.Name = "lbl_time";
+            this.lbl_time.Size = new System.Drawing.Size(54, 20);
+            this.lbl_time.TabIndex = 4;
+            this.lbl_time.Text = "00:00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(477, 10);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Timer :";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // frm_Writing_code_for_run
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(1019, 598);
+            this.ClientSize = new System.Drawing.Size(1080, 754);
+            this.ControlBox = false;
+            this.Controls.Add(this.pnl_conntrol_box);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.txtError);
@@ -151,8 +252,14 @@
             this.Controls.Add(this.btnRun);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtCode);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
             this.Name = "frm_Writing_code_for_run";
+            this.Load += new System.EventHandler(this.frm_Writing_code_for_run_Load);
+            this.pnl_conntrol_box.ResumeLayout(false);
+            this.pnl_conntrol_box.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -169,5 +276,13 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         public System.Windows.Forms.RichTextBox txtError;
+        private System.Windows.Forms.Panel pnl_conntrol_box;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Button btn_Minimize;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbl_time;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- Mars_Languge/Main/Forms/frm_Writing_code_for_run.cs
+++ Mars_Languge/Main/Forms/frm_Writing_code_for_run.cs
@@ -1,24 +1,46 @@
 ﻿using Mars_Language.Main.Class_dll;
 using System;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
 
 namespace Mars_Languge.Main.Forms
 {
+    // ایمپورت توابع ویندوز
+    
+
     public partial class frm_Writing_code_for_run : Form
     {
         public frm_Writing_code_for_run()
         {
             InitializeComponent();
+
         }
+        //--------------control panell-------------------------------------------
+        // ایمپورت توابع ویندوز
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HTCAPTION = 0x2;
+
+        [DllImport("User32.dll")]
+        public static extern bool ReleaseCapture();
+
+        [DllImport("User32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        //---------------------------------------------------------
+
         //Introducing the necessary classes //  معرفی کلاس های لازم
         cl_Lexer Lexer = new cl_Lexer();
-        
+        private int secondsElapsed = 0; // تعداد ثانیه‌های گذشته
         //-------------------------------------------------------
 
         //button Run project/languge----دکمه اجرا پروژه/زبان----------------------------------
         private void btnRun_Click(object sender, EventArgs e)
         {
-           
+            txtError.Clear();  // پاک کردن خطاهای قبلی
+            txtOutput.Clear(); // پاک کردن خروجی قبلی
+            txt_lexser.Clear(); // پاک کردن توکن‌ها
+
             try
             {
                 // مرحله 1: Lexer
@@ -26,23 +48,60 @@ namespace Mars_Languge.Main.Forms
                 var tokens = lexer.Tokenize();
 
                 // نمایش توکن‌ها
-                txtOutput.Clear();
-                txt_lexser.Clear();
                 foreach (var token in tokens)
                     txt_lexser.AppendText(token.ToString() + Environment.NewLine);
 
                 // مرحله 2: Parser و اجرای کد
-                txtOutput.Text = "OutPut >>>  ";
-                var parser = new cl_Parser(tokens, txtOutput); // RichTextBox
+                var parser = new cl_Parser(tokens, txtOutput); // RichTextBox خروجی
                 parser.Parse();
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message, "Mars Language Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // نمایش خطاها در txtError به جای MessageBox
+                txtError.AppendText(ex.Message + Environment.NewLine);
+            }
+        
+
+    }
+
+        private void frm_Writing_code_for_run_Load(object sender, EventArgs e)
+        {
+            secondsElapsed = 0;
+            timer1.Start(); // تایمر رو روشن کن
+
+        }
+
+        
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
             }
         }
 
-    
-    //------------------------------------------------------------------------------------
-}
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            secondsElapsed++;
+
+            // تبدیل ثانیه به hh:mm:ss
+            TimeSpan time = TimeSpan.FromSeconds(secondsElapsed);
+            lbl_time.Text = time.ToString(@"hh\:mm\:ss");
+        }
+
+
+        //------------------------------------------------------------------------------------
+    }
 }
